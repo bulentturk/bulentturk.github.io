fb1d3fda0f884d026fb88db81f98b9a85592b8fd
bulentturk <38316610+bulentturk@users.noreply.github.com>
2026-09-13 09:35:06 +0300

    Render the updated DBC guide without changing other guide routes

--- source/src/entries/guide.tsx
+++ source/src/entries/guide.tsx
@@ -1,5 +1,6 @@
 import GuidePage, { type GuideSlug } from "../GuidePage";
+import DbcGuidePage from "../DbcGuidePage";
 import { mount } from "./mount";
 
 const slug = window.location.pathname.split("/").filter(Boolean).at(-1) as GuideSlug;
-mount(<GuidePage slug={slug} />);
+mount(slug === "dbc-dosyasi-nedir" ? <DbcGuidePage /> : <GuidePage slug={slug} />);
